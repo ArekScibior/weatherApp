463a00681d72a591a32a3b72e865ce0a5ea397be
Arczi14 <arekc247@gmail.com>
2018-01-18 15:39:25 +0100

    https

--- js/services/dataProvider.js
+++ js/services/dataProvider.js
@@ -1,7 +1,7 @@
 angular.module('app')
 .factory('dataProvider', ["$http", "utils",
     function dataProvider($http, utils) {
-        var url ="http://api.apixu.com/v1/current.json?key=7aeed42ae9eb4f47bc8185433170310&q=";
+        var url ="https://api.apixu.com/v1/current.json?key=7aeed42ae9eb4f47bc8185433170310&q=";
         return {
             getResults: function(city) {
                 let promise = $http.get(url + city)
